dea7b10a21f244e7397597e2c632987fe0c6ef66
SZN <sznckz90@gmail.com>
2026-02-06 15:36:05 +0530

    Add files via upload

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,13 +5,15 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Dashboard from "@/pages/Dashboard";
+import Home from "@/pages/Home";
 import Users from "@/pages/Users";
 import Withdrawals from "@/pages/Withdrawals";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Dashboard} />
+      <Route path="/" component={Home} />
+      <Route path="/admin" component={Dashboard} />
       <Route path="/users" component={Users} />
       <Route path="/withdrawals" component={Withdrawals} />
       <Route component={NotFound} />
